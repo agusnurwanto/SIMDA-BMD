6859378db50df657fb7b2f88e8c580463bbfeb79
bagusalvin123 <bagusalvin123>
2024-08-23 12:44:47 +0700

    perbaikan select unit rekap kib c

--- public/partials/wp-simda-bmd-halaman-laporan-rekap-kib-c.php
+++ public/partials/wp-simda-bmd-halaman-laporan-rekap-kib-c.php
@@ -9,7 +9,7 @@ $results = $wpdb->get_results("
     SELECT 
         nama_skpd, 
         kode_skpd,
-        nama_lokasi, 
+        nama_unit, 
         kode_lokasi,
         SUM(CASE WHEN klasifikasi = 'Intracountable' THEN nilai_perolehan ELSE 0 END) AS intra_countable,
         SUM(CASE WHEN klasifikasi = 'Ekstracountable' THEN nilai_perolehan ELSE 0 END) AS ekstra_countable
@@ -57,7 +57,7 @@ foreach ($data_all as $row) {
 	    $body .= '<td class="text-left">' . esc_html($row['kode']) . '</td>';
 	    $body .= '<td class="text-left">' . esc_html($row['nama']) . '</td>';
 	    $body .= '<td class="text-left">' . esc_html($row2->kode_lokasi) . '</td>';
-	    $body .= '<td class="text-left">' . esc_html($row2->nama_lokasi) . '</td>';
+	    $body .= '<td class="text-left">' . esc_html($row2->nama_unit) . '</td>';
 	    $body .= '<td class="text-right">' . number_format($row2->intra_countable,0,",",".").'</td>';
 	    $body .= '<td class="text-right">' . number_format($row2->ekstra_countable,0,",",".") . '</td>';
 	    $body .= '</tr>';
